183067c88bae002bb51d9bef8423f6c3b0797495
Vinson <vinson.yw@gmail.com>
2022-11-17 07:15:15 +0800

    Add dayjs / Guid ts

--- V1/src/types/Property.ts
+++ V1/src/types/Property.ts
@@ -1,4 +1,8 @@
+import type { Guid } from 'guid-ts';
+type GUID = string & { isGuid: true};
 export interface IProperty {
+    Sid:number,
+    FkCaseNo:GUID,
     type: string;
     area: number;
     price: number;
